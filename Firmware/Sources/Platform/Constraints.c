6c695968b08c0b6d71df8aef73a9a334e6329692
mlhzv2204 <rsln.mlhzv@gmail.com>
2021-08-26 17:16:54 +0300

    Фикс баги с уставкой для младшего тока управления

--- Firmware/Sources/Platform/Constraints.c
+++ Firmware/Sources/Platform/Constraints.c
@@ -118,12 +118,12 @@ const TableItemConstraint NVConstraint[DATA_TABLE_NV_SIZE] =
 											{0, 0, 0},																// 102
 											{0, 0, 0},																// 103
 											{0, 0, 0},																// 104
-											{0, INT16U_MAX, 0},																// 105
-											{PROPORTIONAL_MIN, INT16U_MAX, PROPORTIONAL_DEF},																// 106
-											{0, INT16U_MAX, 0},																// 107
-											{PROPORTIONAL_MIN, INT16U_MAX, PROPORTIONAL_DEF},																// 108
-											{0, INT16U_MAX, 0},																// 109
-											{0, 0, 0},																// 110
+											{0, INT16U_MAX, 0},														// 105
+											{PROPORTIONAL_MIN, INT16U_MAX, PROPORTIONAL_DEF},						// 106
+											{0, INT16U_MAX, 0},														// 107
+											{PROPORTIONAL_MIN, INT16U_MAX, PROPORTIONAL_DEF},						// 108
+											{0, INT16U_MAX, 0},														// 109
+											{0, INT16U_MAX, 50},																// 110
 											{0, 0, 0},																// 111
 											{0, 0, 0},																// 112
 											{0, 0, 0},																// 113
