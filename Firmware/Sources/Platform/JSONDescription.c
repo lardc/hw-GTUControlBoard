5f34e0b7c8b19cd3b99c8a0b1d48fb33a8f5f4b3
Pumukun <demgrisha@gmail.com>
2025-02-11 11:12:40 +0300

    Подвязаны библиотеки для работы с json, добавлен шаблон

---
+++ Firmware/Sources/Platform/JSONDescription.c
@@ -0,0 +1,92 @@
+#include "JSONDescription.h"
+#include "FormatOutputJSON.h"
+
+const char TemplateJSON[] = "[{\n"
+"	'param' : 'Vgt',\n"
+"	'type' : 'measure',\n"
+"	'ranges': [\n"
+"		{\n"
+"			'rangeId' : '1',\n"
+"			'unitsMultiply' : '1'\n"
+"			'active' : '1',\n"
+"			'min': '100',\n"
+"			'max': '$'\n"
+"		},\n"
+"		{\n"
+"			'rangeId' : '2',\n"
+"			'unitsMultiply' : '1',\n"
+"			'active' : '1',\n"
+"			'min': '$',\n"
+"			'max': '5000'\n"
+"		}]\n"
+"},\n"
+"{\n"
+"	'param' : 'Igt',\n"
+"	'type' : 'set',\n"
+"	'ranges': [\n"
+"		{\n"
+"			'rangeId' : '3',\n"
+"			'unitsMultiply' : '1',\n"
+"			'active' : '1',\n"
+"			'min': '10',\n"
+"			'max': '$'\n"
+"		},\n"
+"		{\n"
+"			'rangeId' : '4',\n"
+"			'unitsMultiply' : '1',\n"
+"			'active' : '1',\n"
+"			'min': '$',\n"
+"			'max': '$'\n"
+"		}]\n"
+"},\n"
+"{\n"
+"	'param' : 'Ih',\n"
+"	'type' : 'set',\n"
+"	'ranges': [\n"
+"		{\n"
+"			'rangeId' : '5',\n"
+"			'unitsMultiply' : '1',\n"
+"			'active' : '1',\n"
+"			'min': '$',\n"
+"			'max': '$'\n"
+"		}]\n"
+"},\n"
+"{\n"
+"	'param' : 'IL',\n"
+"	'type' : 'set',\n"
+"	'ranges': [\n"
+"		{\n"
+"			'rangeId' : '6',\n"
+"			'unitsMultiply' : '1',\n"
+"			'active' : '1',\n"
+"			'min': '50',\n"
+"			'max': '$'\n"
+"		}]\n"
+"},\n"
+"{\n"
+"	'param' : 'Rg',\n"
+"	'type' : 'measure',\n"
+"	'ranges': [\n"
+"		{\n"
+"			'rangeId' : '7',\n"
+"			'unitsMultiply' : '1',\n"
+"			'active' : '1',\n"
+"			'min': '2',\n"
+"			'max': '30'\n"
+"		}]\n"
+"}]";
+
+Int16U Vgt1Max;
+Int16U Igt1Max, Igt2Max;
+Int16U Ih1Min, Ih1Max;
+Int16U IL1Max;
+
+
+Int16U JSONPointers[JSON_POINTERS_SIZE] = {0};
+
+void JSON_AssignPointer(Int16U Index, Int32U Pointer)
+{
+	if (Index < JSON_POINTERS_SIZE)
+		JSONPointers[Index] = Pointer;
+}
+
---
+++ Firmware/Sources/Platform/JSONDescription.h
@@ -0,0 +1,17 @@
+#ifndef __JSON_DESCRIPTION_H
+#define __JSON_DESCRIPTION_H
+
+#include "Global.h"
+
+#define JSON_POINTERS_SIZE 32
+
+extern Int16U Vgt1Max;
+extern Int16U Igt1Max, Igt2Max;
+extern Int16U Ih1Min, Ih1Max;
+extern Int16U IL1Max;
+
+extern Int16U JSONPointers[JSON_POINTERS_SIZE];
+
+void JSON_AssignPointer(Int16U Index, Int32U Pointer);
+
+#endif // __JSON_DESCRIPTION_H
